506ab7fc23cb8ef5d895661828199200914048a6
Rohan Kadam <rohan.kadam@mindbowser.com>
2024-07-07 18:41:22 +0530

    call apply bind done

---
+++ callApplyBind.js
@@ -0,0 +1,34 @@
+/** CALL */
+
+// Call is a function that helps you change the context of the invoking function. In layperson's terms, it helps you replace the value of this inside a function with whatever value you want.
+
+const newPerson = {
+    name: "sid kadam",
+    email: "sid@gmail.com",
+}
+
+const person1 = {
+    name: "rohan kadam",
+    email: "r@gmail.com",
+    print: function () {
+        console.log(`Hello ${this.name}`)
+    }
+}
+
+// person1.print.call(newPerson)
+
+/** CALL ENDS */
+
+
+/** APPLY */
+// Apply is very similar to the call function. The only difference is that in apply you can pass an array as an argument list.
+
+
+// person1.print.apply(newPerson, [1, 2, 3])
+
+
+/** BIND */
+// Bind is a function that helps you create another function that you can execute later with the new context of this that is provided.
+
+const newFunc = person1.print.bind(newPerson, [1, 2, 3])
+newFunc()
---
+++ polyFills.js
@@ -0,0 +1,20 @@
+/** FILTER */
+
+// const arr = [2, 3, 6, 7, 8, 3, 4]
+
+// Array.prototype.myFilter = function (cb) {
+//     const temp = []
+//     for (let i = 0; i < this.length; i++) {
+//         if (cb(this[i])) {
+//             temp.push(this[i])
+//         }
+//     }
+//     return temp
+// }
+
+// const filtered = arr.myFilter((item) => {
+//     return item % 2 === 0
+// })
+
+// console.log(filtered)
+/** FILTER */
